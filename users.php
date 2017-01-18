234e163bbb2f2707942a5608c31eb621c2a006f8
Laure <laure.milian@gmail.com>
2017-01-18 10:14:04 +0100

    utilisation de l'objet date pour propriete created at

--- users.php
+++ users.php
@@ -2,8 +2,8 @@
 require 'client.php';
 
 
-	$client1 = new Client("client1@mail.com", "client1", 111111);
-	$client2 = new Client("client2@gmail.com", "client2", 222222);
+	$client1 = new Client("client1@mail.com", "client1", date('l jS \of F Y h:i:s A'));
+	$client2 = new Client("client2@gmail.com", "client2", date('l jS \of F Y h:i:s A'));
 
 	$users = [$client1, $client2];
 
