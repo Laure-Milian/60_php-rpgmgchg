ec9b2353743e1d06e910255b49987d9fe49263e0
Laure <laure.milian@gmail.com>
2017-01-20 14:45:54 +0100

    ajout des select de produits + bouton d'envoi de commande

--- shopping.php
+++ shopping.php
@@ -1,4 +1,5 @@
 <?php $clients = require $_SERVER['DOCUMENT_ROOT'] . '/data/users.php'; ?>
+<?php $products = require $_SERVER['DOCUMENT_ROOT'] . '/data/products.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,25 +8,49 @@
 </head>
 <body>
 
-<?php var_dump($clients); ?>
+	<form action="/" method="post">
 
-<select name="selectClient" id="selectClient">
-	<?php foreach ($clients as $client): ?>
-		<label for="selectClient"></label>
-			<option value="<?= $client->getId() ?>"> <?= $client->getId(); ?> </option>
-	<?php endforeach ?>
-</select>
+		<div>
+			<label for="selectClient">Sélectionner un client : </label>
+			<select name="selectClient" id="selectClient">
+				<?php foreach ($clients as $client): ?>
+					<option value="<?= $client->getId() ?>"> <?= $client->getId(); ?> </option>
+				<?php endforeach ?>
+			</select>
+		</div>
 
+		<div>
+			<label for="selectProduct1">Sélection un 1er produit :</label>
+			<select name="selectProduct1" id="selectProduct1">
+				<?php foreach ($products as $product): ?>
+					<option value="<?= $product->getName() ?>"> <?= $product->getName(); ?> </option>
+				<?php endforeach ?>
+			</select>
+		</div>
 
+		<div>
+			<label for="selectProduct2">Sélection un 2è produit :</label>
+			<select name="selectProduct2" id="selectProduct2">
+				<?php foreach ($products as $product): ?>
+					<option value="<?= $product->getName() ?>"> <?= $product->getName(); ?> </option>
+				<?php endforeach ?>
+			</select>
+		</div>
+
+		<div>
+			<label for="selectProduct3">Sélection un 3è produit :</label>
+			<select name="selectProduct3" id="selectProduct3">
+				<?php foreach ($products as $product): ?>
+					<option value="<?= $product->getName() ?>"> <?= $product->getName(); ?> </option>
+				<?php endforeach ?>
+			</select>
+		</div>
+
+		<div>
+			<input type="submit" value="Envoyer la commande">
+		</div>
+
+	</form>
 	
 </body>
 </html>
-
-Créer une page shopping.php qui va permettre de créer une commande pour un client.
-
-Afficher un select qui va permettre d'afficher et de sélectionner un client.
-
-Afficher 3 select successifs qui vont permettre de sélectionner chacun un produit.
-
-Ajouter un bouton d'envoi de commande.
-
