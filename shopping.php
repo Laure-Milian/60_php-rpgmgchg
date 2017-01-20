78828e18704f747061a8370c0d2ab952380f0e03
Laure <laure.milian@gmail.com>
2017-01-20 13:59:20 +0100

    creation du select clients

--- shopping.php
+++ shopping.php
@@ -1,3 +1,31 @@
-<?php
+<?php $clients = require $_SERVER['DOCUMENT_ROOT'] . '/data/users.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Shopping Time</title>
+</head>
+<body>
 
-include '/data/users.php';
+<?php var_dump($clients); ?>
+
+<select name="selectClient" id="selectClient">
+	<?php foreach ($clients as $client): ?>
+		<label for="selectClient"></label>
+			<option value="<?= $client->getId() ?>"> <?= $client->getId(); ?> </option>
+	<?php endforeach ?>
+</select>
+
+
+	
+</body>
+</html>
+
+Créer une page shopping.php qui va permettre de créer une commande pour un client.
+
+Afficher un select qui va permettre d'afficher et de sélectionner un client.
+
+Afficher 3 select successifs qui vont permettre de sélectionner chacun un produit.
+
+Ajouter un bouton d'envoi de commande.
+
