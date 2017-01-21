8dcff7df36e6e9fc9177c39d099b5373ce00289c
Laure <laure.milian@gmail.com>
2017-01-21 23:51:31 +0100

    step 5 complete

--- views/validateOrder.php
+++ views/validateOrder.php
@@ -3,13 +3,53 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Validation de commande</title>
-	<?php $products = require_once $_SERVER['DOCUMENT_ROOT'] . '/data/products.php'; ?>
-	<?php $clients = require_once $_SERVER['DOCUMENT_ROOT'] . '/data/users.php'; ?>
 </head>
 <body>
 
 	<?php 
+	$clients = require_once $_SERVER['DOCUMENT_ROOT'] . '/data/users.php';
+	$products = require $_SERVER['DOCUMENT_ROOT'] . '/data/products.php';
+	
+	// Retrouver les produits correspondants aux ID sélectionnés
+	
+	$selected_products = [$_POST["selectProduct1"], $_POST["selectProduct2"], $_POST["selectProduct3"]];
+	
+	$current_products = array_map(function($selectProductId) use ($products) {
+		foreach ($products as $p) {
+			if ($selectProductId === $p->getId()) {
+				return $p;
+			}
+		}
+	}, $selected_products);
+
+	// Retrouver le client correspondant à l'ID sélectionné
+	
 	$idClient = $_POST["selectClient"];
+	
+	function findClient($idClient, $clients) {
+		foreach ($clients as $client) {
+			if ($idClient === $client->getId()) {
+				return $client;
+			}
+		}
+	}
+
+	$currentClient = findClient($idClient, $clients);
+
+	// Utiliser buy() sur chaque produit sélectionné
+	echo 'Liste des produits achetés : ';
+	foreach ($current_products as $cp) {
+		echo '<div>' . $cp->getName() . '</div>';
+		$currentClient->buy($cp);
+	}
+
+	echo '<div> Panier total :' . ($currentClient->getBillAmount()) . '€ </div>';
+
+
+
+
+	/*$idClient = $_POST["selectClient"];
+
 	$selected_products = [$_POST["selectProduct1"], $_POST["selectProduct2"], $_POST["selectProduct3"]]; 
 
 	function findProducts($selected_products, $products) {
@@ -24,15 +64,11 @@
 		}
 	}
 
-	var_dump(findProducts($selected_products, $products));
 
+	//array_map("findProducts", $selected_products)
 
+	var_dump(findProducts($selected_products, $products));*/
 
-	/*foreach ($clients as $client) {
-		if ($idClient === $client->getId()) {
-			$client->buy($product1);
-		}
-	}*/
 
 	?>
 	
